f0612e8d20781be9fdd110fd5aa01811ec4622d8
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-06-19 09:50:45 -0300

    Adicionando Componentes Container e Titulo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Cabecalho from './components/Cabecalho';
+import Container from './components/Container';
+import Titulo from './components/Titulo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Cabecalho />
+      <Container>
+        <Titulo>Área Administrativa</Titulo>
+      </Container>
+    </>
   );
 }
 
