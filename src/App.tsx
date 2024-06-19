2977733802aa16814932fcf98b8e10ab174c07ee
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-06-19 14:41:44 -0300

    Adicionando tabela

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import Cabecalho from './components/Cabecalho';
 import Container from './components/Container';
 import Titulo from './components/Titulo';
 import { Rodape } from './components/Rodape';
+import Tabela from './components/Tabela';
+import useDadosConsulta from './useDadosConsulta';
 
 function App() {
+  const { dados, erro } = useDadosConsulta()
   return (
     <>
       <Cabecalho />
       <Container>
         <Titulo>Área Administrativa</Titulo>
+        <Tabela consultas={dados} />
       </Container>
       <Rodape />
     </>
